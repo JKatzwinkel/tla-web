a2010e84f0de1520aa613ccffb482ebfcd7818a2
Jakob Hoeper <JKatzwinkel@users.noreply.github.com>
2022-07-15 06:04:53 +0200

    e2e: disable video recording

--- src/test/e2e/cypress.config.js
+++ src/test/e2e/cypress.config.js
@@ -5,7 +5,8 @@ module.exports = defineConfig(
     e2e: {
       baseUrl: "http://127.0.0.1:8080",
       supportFile: false,
-      specPattern: "cypress/**/*.spec.js"
+      specPattern: "cypress/**/*.spec.js",
+      video: false
     }
   }
 )
