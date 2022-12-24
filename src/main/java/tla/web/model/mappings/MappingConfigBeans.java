cb238685c9d5bb4571bb9a94a343676b2976b1f3
jkatzwinkel <JKatzwinkel@users.noreply.github.com>
2022-12-24 13:14:04 +0100

    mv converter bean into own spring component to prevent dependency cycle

---
+++ src/main/java/tla/web/model/mappings/MappingConfigBeans.java
@@ -0,0 +1,18 @@
+package tla.web.model.mappings;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import tla.web.config.ApplicationProperties;
+
+@Configuration
+public class MappingConfigBeans {
+
+    @Bean
+    public ExternalReferencesConverter externalReferencesConverter(
+        ApplicationProperties properties
+    ) {
+        return new ExternalReferencesConverter(properties);
+    }
+
+}
