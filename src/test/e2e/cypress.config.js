b8f89179b8bc5cae5ce64bcf8602639f0efa5a03
JKatzwinkel <JKatzwinkel@users.noreply.github.com>
2023-10-01 15:21:39 +0000

    e2e: install cypress terminal-report plugin

--- src/test/e2e/cypress.config.js
+++ src/test/e2e/cypress.config.js
@@ -3,8 +3,10 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig(
   {
     e2e: {
+      setupNodeEvents(on) {
+        require('cypress-terminal-report/src/installLogsPrinter')(on)
+      },
       baseUrl: "http://127.0.0.1:8080",
-      supportFile: false,
       specPattern: "cypress/**/*.spec.js",
       video: false
     }
