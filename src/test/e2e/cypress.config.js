8d912d18ae7abb8e555d12beea882ef5e8e2dec1
JKatzwinkel <JKatzwinkel@users.noreply.github.com>
2024-02-11 12:53:27 +0000

    e2e: always print logs to terminal
    not only for failing tests

--- src/test/e2e/cypress.config.js
+++ src/test/e2e/cypress.config.js
@@ -4,7 +4,9 @@ module.exports = defineConfig(
   {
     e2e: {
       setupNodeEvents(on) {
-        require('cypress-terminal-report/src/installLogsPrinter')(on)
+        require('cypress-terminal-report/src/installLogsPrinter')(
+          on, { printLogsToConsole: 'always' }
+        )
       },
       baseUrl: "http://127.0.0.1:8080",
       specPattern: "cypress/**/*.spec.js",
